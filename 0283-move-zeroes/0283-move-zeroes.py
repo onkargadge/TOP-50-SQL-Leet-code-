f48f07ececac0b312bc35334d6c12cc738b58904
Onkar Gadge <160859878+onkargadge@users.noreply.github.com>
2026-09-03 13:00:36 +0530

    Time: 367 ms (8.4%), Space: 13.3 MB (88.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        final=[]
         count = 0 
-        for i in nums:
+        for i in nums[:]:
             if i == 0:
                 count+=1
-            else:
-                final.append(i)
-        for i in range(count):
-            final.append(0)
-        nums[:] = final
+                nums.remove(i)
+        for j in range(count):
+            nums.append(0)
+        return nums
